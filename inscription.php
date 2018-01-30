59bd83d5db23902169cb39475d86a4898c8cbfcb
stayx <stanley.tissot@ynov.com>
2018-01-30 14:59:23 +0100

    Add files via upload

--- inscription.php
+++ inscription.php
@@ -1,7 +1,7 @@
 <?php
 require('./includes/init.php');
 include('./includes/header.html');
-include ('./includes/footer.html');
+
 
 ?>
 
@@ -70,6 +70,10 @@ if ((isset($_POST)) && (!empty($_POST['username'])) && (!empty($_POST['last_name
     }
 }
 
+?>
+<div class=""></div>
+<?php
+include ('./includes/footer.html');
 ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
